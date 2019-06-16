e4b0e412e39dd94fd445521e6aa9c36ad6013c9b
John P. Willis <jpw@coherent-logic.com>
2019-06-15 23:01:38 -0600

    Modularize package

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+var p = require('say-letter-p');
+var o = require('say-letter-o');
+var period = require('say-letter-period');
+var newline = require('say-letter-newline');
+
 module.exports = sayPoop;
 
 function sayPoop() {
-    console.log("poop.");
+    p();
+    o();
+    o();
+    p();
+    period();
+    newline();
 }
